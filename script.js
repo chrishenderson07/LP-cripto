db69651669e7117a60e714d0ed861162b33cd8e3
nerodesigndev <chris.hend012@gmail.com>
2022-06-16 13:22:55 -0300

    Implementação de Menu Responsivo

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-    let button = getElementByQuerySelector(buttonMenu)
-    button.innerHTML="<p>Teste de mensagem</p>"
+const btnMobile = document.getElementById('btn-mobile')
+const loginMenu = document.getElementById('menu')
+
+function toggleMenu(event){
+    if (event.type === 'touchstart') event.preventDefault()
+    const nav = document.querySelector(".topbar")
+    nav.classList.toggle('active')
+    const active = nav.classList.contains('active')
+    nav.style.backgroundColor = "#fff00"
+    event.currentTarget.setAttribute('aria-expanded', active)    
+}
+
+btnMobile.addEventListener('click', toggleMenu)
+btnMobile.addEventListener('touchstart', toggleMenu)
