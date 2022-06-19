e5da6b53925155d0a75dc2322da227f40ddbabc7
nerodesigndev <chris.hend012@gmail.com>
2022-06-19 09:21:32 -0300

    Correção Button Mobile

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const btnMobile = document.getElementById('btn-mobile')
-const loginMenu = document.getElementById('menu')
+const btnMobile = document.querySelector('#btn-mobile')
 
 function toggleMenu(event){
-    if (event.type === 'touchstart') event.preventDefault()
-    const nav = document.querySelector(".topbar")
+    if(event.type === 'touchstart') event.preventDefaut()
+    const nav = document.querySelector('.topbar')
     nav.classList.toggle('active')
     const active = nav.classList.contains('active')
-    nav.style.backgroundColor = "#fff00"
-    event.currentTarget.setAttribute('aria-expanded', active)    
+    nav.style.backgroundColor = "#fff00"    
+    event.currentTarget.setAttribute('aria-expanded', active)
 }
+
 
 btnMobile.addEventListener('click', toggleMenu)
 btnMobile.addEventListener('touchstart', toggleMenu)
